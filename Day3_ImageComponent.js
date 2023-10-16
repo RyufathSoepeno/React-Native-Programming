be3b7c91b6f51b489667748b553a0e6842f8cbc3
Ryufath Alief Adhyaksa Putera Soepeno <89006072+ryu0210@users.noreply.github.com>
2023-10-16 11:06:36 +0700

    Create Day3_ImageComponent.js

---
+++ Day3_ImageComponent.js
@@ -0,0 +1,44 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+
+/** There are two ways to load images
+ * Method 1: Upload image to explorer in VS Code, local images
+ * Here, we do not need to use <image></image>, we can make it more tidier by using <image />
+ * 
+ * Example ----> <image source={require('./assets/icon.png')}/>
+ * 
+ *
+ * Method 2: Use Network, beyond VS code local images
+ * 
+ * For this one, you have to specify the width and height of the images, unlike local
+ * Pay attention to code below
+ */
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Well done, you've made it!</Text>
+      <image 
+      fadeDuration={1000} /// Fade Duration is in milliseconds
+      
+      source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300",
+      }}
+      />
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'dodgerblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
