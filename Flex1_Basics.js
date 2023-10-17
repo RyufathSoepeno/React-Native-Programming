5acb581d5ac4089fb929f591bbe013850be3410e
Ryufath Alief Adhyaksa Putera Soepeno <89006072+ryu0210@users.noreply.github.com>
2023-10-17 15:19:52 +0700

    Create Flex1_Basics.js

---
+++ Flex1_Basics.js
@@ -0,0 +1,35 @@
+import React from "react"
+
+import { View } from 'react-native';
+
+/** like arranging items in a row or column, and they automatically adjust their size to fit the available space. 
+ * It's great for creating responsive designs and aligning content within a container */
+
+ /// flex: 0.5 takes half, you do the maths
+
+export default function App() {
+  return (
+    <View style={{
+      backgroundColor: "darksalmon",
+      flex: 1,
+      }
+    }> 
+
+      <View style={{
+        backgroundColor: "darksalmon",
+        flex: 1,
+      }}/>
+      
+      <View style={{
+        backgroundColor: "cornsilk",
+        flex: 2,
+      }}/>
+
+      <View style={{
+        backgroundColor: "darkorchid",
+        flex: 1,
+      }}/>
+
+    </View>
+  );
+}
